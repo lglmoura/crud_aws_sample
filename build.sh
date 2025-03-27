fa54a028f48d2c830d81f6f91bb4f40e4170f546
Luiz Gustavo Lourenço Moura <lglmoura@gmail.com>
2025-03-27 16:02:01 -0300

    chore:criando ecr

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 ECR_REGISTRY="751631479229.dkr.ecr.us-east-1.amazonaws.com"
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REGISTRY
-docker build -t aws_aula .
-docker tag aws_aula:latest $ECR_REGISTRY/aws_aula:latest
-docker push $ECR_REGISTRY/aws_aula:latest
+docker build -t ecr-aws-aula .
+docker tag ecr-aws-aula:latest $ECR_REGISTRY/ecr-aws-aula:latest
+docker push $ECR_REGISTRY/ecr-aws-aula:latest
